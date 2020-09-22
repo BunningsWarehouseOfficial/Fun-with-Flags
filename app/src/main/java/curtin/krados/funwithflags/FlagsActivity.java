87860beca6446dd843b3b3f9ce175b3886de8841
BunningsWarehouseOfficial <kristianrados40@gmail.com>
2020-09-22 11:52:54 +0800

    feat: implement MainActivity
    Signed-off-by: BunningsWarehouseOfficial <kristianrados40@gmail.com>

--- app/src/main/java/curtin/krados/funwithflags/FlagsActivity.java
+++ app/src/main/java/curtin/krados/funwithflags/FlagsActivity.java
@@ -1,4 +1,26 @@
 package curtin.krados.funwithflags;
 
-public class FlagsActivity {
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FlagsActivity extends AppCompatActivity {
+    private static final String START = "curtin.krados.funwithflags.start";
+    private static final String TARGET = "curtin.krados.funwithflags.target";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flags);
+    }
+
+    //Decoupling method for starting the activity
+    public static Intent getIntent(Context c, int startPoints, int targetPoints) {
+        Intent intent = new Intent(c, FlagsActivity.class);
+        intent.putExtra(START, startPoints);
+        intent.putExtra(TARGET, targetPoints);
+        return intent;
+    }
 }
--- app/src/main/java/curtin/krados/funwithflags/MainActivity.java
+++ app/src/main/java/curtin/krados/funwithflags/MainActivity.java
@@ -2,14 +2,51 @@ package curtin.krados.funwithflags;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private int mStartPoints;
+    private int mTargetPoints;
+
+    private ImageView mAppLogo;
+    private TextView mStartPointsText;
+    private TextView mTargetPointsText;
+    private Button mStartButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initialising point values
+        Random rand = new Random(); //Create a seed
+        mStartPoints = rand.nextInt(15 - 5) + 5;
+        mTargetPoints = rand.nextInt(35 - 25) + 25;
+
+        //Retrieving references
+        mAppLogo = (ImageView) findViewById(R.id.appLogo);
+        mStartPointsText = (TextView) findViewById(R.id.startPointsText);
+        mTargetPointsText = (TextView) findViewById(R.id.targetPointsText);
+        mStartButton = (Button) findViewById(R.id.startButton);
+
+        mStartPointsText.setText(getString(R.string.start_points, mStartPoints));
+        mTargetPointsText.setText(getString(R.string.target_points, mTargetPoints));
+
+        //Implementing callbacks and setting up event handlers
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = FlagsActivity.getIntent(MainActivity.this, mStartPoints, mTargetPoints);
+                startActivity(intent);
+            }
+        });
     }
 }
 
@@ -21,3 +58,4 @@ public class MainActivity extends AppCompatActivity {
 
 //LOW PRIORITY
 //TODO Set content description of layout selector images to change as the images themselves change
+//TODO Put layout files into directories for each activity (see GeoQuiz and textbook)
